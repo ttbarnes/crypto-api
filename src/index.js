6936f4f200358cf049a13e4a9c538f36d81cd48c
ttbarnes <tony@tonybarnes.me>
2017-12-28 23:46:21 +0000

    fix typo

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ initializeDb( db => {
 	// app.use(middleware({ config, db }));
 
 	app.use(function (req, res, next) {
-		res.header('Access-Control-Allow-Origin', 'http://http://localhost:3000');
+		res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
 		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 		next();
 	});
