4be4e2ccb495405f92625ecb90c80b86e89cd310
ttbarnes <tony@tonybarnes.me>
2017-12-30 22:27:55 +0000

    throw error if token expired

--- src/api/token.js
+++ src/api/token.js
@@ -17,32 +17,40 @@ const getToken = (headers) => {
 export function checkTokenGetUserData(req, res) {
   const token = getToken(req.headers);
   if (token) {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    return User.findOne({
-      username: decoded.data.username
-    }, (err, user) => {
-      if (err) throw err;
-
-      if (!user) {
-        return res.status(403).send({ errorCheckingToken: true });
+    return jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        if (err.message === 'jwt expired') {
+          return res.status(401).send({ success: false, message: 'token expred' });
+        }
       }
-      if (user) {
-        let decryptedKeys = [];
-        user.keys.map((k) => {
-          let decryptedObj = k;
-          decryptedObj.key = decrypt(k.key);
-          decryptedObj.secret = decrypt(k.secret);
-          decryptedKeys.push(decryptedObj);
-        });
 
-        const resUserObj = {
-          _id: user._id,
-          username: user.username,
-          keys: decryptedKeys
-        };
-        return res.status(200).send({ success: true, resUserObj });
-      }
-      return err;
+      return User.findOne({
+        username: decoded.data.username
+      }, (err, user) => {
+        if (err) throw err;
+
+        if (!user) {
+          return res.status(403).send({ errorCheckingToken: true });
+        }
+        if (user) {
+          let decryptedKeys = [];
+          user.keys.map((k) => {
+            let decryptedObj = k;
+            decryptedObj.key = decrypt(k.key);
+            decryptedObj.secret = decrypt(k.secret);
+            decryptedKeys.push(decryptedObj);
+          });
+
+          const resUserObj = {
+            _id: user._id,
+            username: user.username,
+            keys: decryptedKeys
+          };
+          return res.status(200).send({ success: true, resUserObj });
+        }
+        return err;
+      });
+
     });
   }
   return res.status(403).send({ success: false, message: 'No token provided' });
